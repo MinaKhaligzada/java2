b2f10fa7c2dfb5e288f0f95562d563d777cb8974
MinaKhaligzada <mina.xaliqova@gmail.com>
2024-08-21 17:29:06 +0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public static void main(String[] args) {
+     int a = 3;
+     int b = 4;
+/*//            if (a>0) System.out.println("Musbet");
+//            else System.out.println("Menfi");
+        int b = 5;
+        if (b>a) System.out.println( b + " boyukdur " + a + " -den");
+
+int c = 6;
+if (c>b>a) System.out.println();*/
+    String test = "";
+    double theDivison = (double) a /b;
+    System.out.println(theDivison
+
+    );
+}
+
+
